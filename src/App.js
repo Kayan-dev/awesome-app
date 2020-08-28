b548a0a745d53527484bef40660fab2d3971b9f3
Kayan-dev <kaya-development@outlook.com>
2020-08-28 15:30:45 +0200

    checking netlify

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello This is netlify
         </a>
       </header>
     </div>
